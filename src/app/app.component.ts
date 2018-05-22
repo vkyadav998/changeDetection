03965ccbfbf6d5aee9325380a2ae9e01c59cd096
bhavesh-neo <bhavesh.vyas@neosofttech.com>
2018-05-22 12:30:44 +0530

    watch detection in angular2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,36 @@
-import { Component } from '@angular/core';
+import { Component,DoCheck,KeyValueChanges, KeyValueDiffer, KeyValueDiffers } from '@angular/core';
+
+export class Customer {
+  firstName: string;
+  favoriteColor: string;
+}
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app';
+
+export class AppComponent implements DoCheck {
+  private customerDiffer: KeyValueDiffer<string, any>;
+  private customer: Customer;
+
+  constructor(private differs: KeyValueDiffers) {}
+
+  ngOnInit(): void {
+    this.customer = new Customer();
+    this.customerDiffer = this.differs.find(this.customer).create();
+  }
+
+  customerChanged(changes: KeyValueChanges<string, any>) {
+    console.log('changes');
+  }
+
+  ngDoCheck(): void {
+      const changes = this.customerDiffer.diff(this.customer);
+      if (changes) {
+        this.customerChanged(changes);
+      }
+  }
+
 }
